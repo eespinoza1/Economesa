e55a6e6bf9791d4c843f9c56e279606fc884b20c
eespinoza1 <co.bordados.riot@gmail.com>
2024-02-26 16:12:13 -0500

    ws

--- main.js
+++ main.js
@@ -39,9 +39,37 @@ document.addEventListener("DOMContentLoaded", function () {
       resultDiv.innerHTML = "Seleccione una moneda válida.";
     } else {
       const result = amount * exchangeRate;
-      resultDiv.innerHTML = `<p>El monto en bolívares venezolanos es: ${result
+      resultDiv.innerHTML = `<h2>El monto en bolívares venezolanos es: ${result
         .toFixed(2)
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>`;
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</h2>`;
     }
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtenemos referencia a los elementos del formulario
+  var emailInput = document.getElementById("emailInput");
+  var submitButton = document.getElementById("submitButton");
+
+  // Añadimos un listener al botón de suscripción
+  submitButton.addEventListener("click", function (event) {
+    // Verificamos si el correo electrónico es válido
+    if (!validateEmail(emailInput.value)) {
+      // Si el correo electrónico no es válido, evitamos que el formulario se envíe
+      event.preventDefault();
+      // Mostramos un mensaje de error
+      alert("Por favor ingrese un correo electrónico válido.");
+    } else {
+      // Si el correo electrónico es válido, cambiamos el valor del input y mostramos un mensaje de suscripción exitosa
+      emailInput.value = "";
+      alert("¡Suscripción exitosa!");
+    }
+  });
+
+  // Función para validar el correo electrónico con una expresión regular
+  function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+});
+
